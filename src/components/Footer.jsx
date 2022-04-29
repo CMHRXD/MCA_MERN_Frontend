71b648695a618a90e0b4aaea9cfa26071f905ba4
Cristian Hernandez <cmhrlxl@gmail.com>
2022-04-29 09:57:51 -0400

    Changing titles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className=' py-10'>
-      <p className=' text-center font-bold'>Administrador de Pacientes de <span className=' text-indigo-500'>Hospital</span></p>
+      <p className=' text-center font-bold'>Administrador de Pacientes de <span className=' text-indigo-500'>Consultorio</span></p>
     </footer>
   )
 }
 
-export default Footer
+export default Footer;
